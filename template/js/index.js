9ce505f9dd7a502fd72092ff9455b12b390fd8d3
yashsuthar010 <83854694+yashsuthar010@users.noreply.github.com>
2021-11-22 17:22:30 +0530

    Add files via upload

---
+++ template/js/index.js
@@ -0,0 +1,18 @@
+
+
+
+
+var logoElement = $('footer .logo');
+
+$(window).scroll(function() {
+  
+   if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+     
+       $(logoElement).addClass('show');
+     
+   } else if($(logoElement).hasClass('show') && $(window).scrollTop() + $(window).height() > $(document).height() - 150) {
+     
+      $(logoElement).removeClass('show');
+     
+   }
+});
